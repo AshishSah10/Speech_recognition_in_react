c895e2582cfd0167247f3544767e7b2fffe92f8f
AshishSah10 <41590024+AshishSah10@users.noreply.github.com>
2021-05-25 12:22:54 +0530

    -commit2

--- src/App.js
+++ src/App.js
@@ -17,14 +17,17 @@ export default function App() {
       setScore(score+1)
       setRandomNumber(randomNumberGenerator)
     }
+    return () => 
+      recognition.removeEventListener("result", this);
   }, [text])
   let recognition = new window.SpeechRecognition();
   recognition.start();
+  window.addEventListener("click", () => console.log("hi"), {once: true})
   recognition.addEventListener("result", (e) => {
     console.log(e.results[0][0].transcript)
     setText(e.results[0][0].transcript)
   })
-  // console.log(recognition)
+
   return (
     <div className="App">
       <h2>Number Game</h2>
